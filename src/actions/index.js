c2fd35c02d531fac99a969fc6da0806c9bcb3863
bwadlow <bwadlow@gmail.com>
2019-03-11 00:41:59 -0400

    action and reducer added

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,8 @@
-import React from 'react'
+
+
+export const selectSong = () => {
+  return {
+    type:"SONG_SELECTED"
+    payload:song
+  }
+}
---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+
+const  songsReducer = () => {
+  return [
+    {title:'Englishmen in New York', duration: '3:05'},
+    {title:'DaDoDoDo, DaDaDaDa', duration:'4:12'},
+    {title:'Every Little Thing She Does', duration:'3:40'},
+    {title:'Canary in a Coal Mine', duration: '3:30'}
+  ];
+}
